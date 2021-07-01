2a3e005256f20bebbde3c11c9b558de8a143f707
thwhawngg <86742675+thwhawngg@users.noreply.github.com>
2021-07-01 07:40:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Nghe em nói này
+    introDesc: `nghe em nói này
     lại là đôi mắt anh
     lạc vào tâm trí em
     chẳng hiểu vì sao cứ nghĩ đến mãi thôi
@@ -13,9 +13,9 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu cũng phải yêu <3333',
-    mess: 'Em biết mà ❤️. Yêu anh nhiều ❤️'
-    messDesc: 'Tối nay 7h đi ts với em nhé .',
+    mess: 'Em biết mà ❤️. Yêu anh nhiều ❤️',
+    messDesc: 'Tối nay 7h đi ts với em nhé',
     btnAccept: 'Oke luôn <3',
-    messLink:https://www.facebook.com/tuyet.nhantruc.392/
+    messLink:'https://www.facebook.com/tuyet.nhantruc.392/',
+}    
     
-    }
