b5f91e13c2edecd5aa257914cc1bee1432b10a14
thwhawngg <86742675+thwhawngg@users.noreply.github.com>
2021-07-01 08:07:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu cũng phải yêu <3333',
     mess: 'Em biết mà ❤️. Yêu anh nhiều ❤️',
-    messDesc: 'Tối nay 7h đi ts với em nhé.',
+    messDesc: 'Tối nay 8h đi ts với em nhé.',
     btnAccept: 'Oke luôn <3',
     messLink: 'https://m.me/tuyet.nhantruc.392'
 }    
