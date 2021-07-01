a6b88bc105517837e7e06d138fd66e6db192f8a5
thwhawngg <86742675+thwhawngg@users.noreply.github.com>
2021-07-01 07:48:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `nghe em nói này
+    introDesc:`nghe em nói này
     lại là đôi mắt anh
     lạc vào tâm trí em
     chẳng hiểu vì sao cứ nghĩ đến mãi thôi
     lỡ say anh mất rồi:)))',
     btnIntro: 'ỏwww:3',
-    title: 'Anh có thương em khonggg 😙',
-    desc: 'Có yêu em khummm:)))',
-    btnYes: 'Có thương em nhất <33',
-    btnNo: 'Không, em à :3',
+    title:'Anh có thương em khonggg 😙',
+    desc:'Có yêu em khummm:)))',
+    btnYes:'Có thương em nhất <33',
+    btnNo:'Không, em à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu cũng phải yêu <3333',
