e74ed0a059e6f200d682db66eb86b4b666504a1c
thwhawngg <86742675+thwhawngg@users.noreply.github.com>
2021-07-01 07:49:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ const CONFIG = {
     chẳng hiểu vì sao cứ nghĩ đến mãi thôi
     lỡ say anh mất rồi:)))',
     btnIntro: 'ỏwww:3',
-    title:'Anh có thương em khonggg 😙',
-    desc:'Có yêu em khummm:)))',
-    btnYes:'Có thương em nhất <33',
-    btnNo:'Không, em à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    title: 'Anh có thương em khonggg 😙',
+    desc: 'Có yêu em khummm:)))',
+    btnYes: 'Có thương em nhất <33',
+    btnNo: 'Không, em à :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu cũng phải yêu <3333',
     mess: 'Em biết mà ❤️. Yêu anh nhiều ❤️',
     messDesc: 'Tối nay 7h đi ts với em nhé',
     btnAccept: 'Oke luôn <3',
-    messLink:'https://m.me/tuyet.nhantruc.392'
+    messLink: 'https://m.me/tuyet.nhantruc.392'
 }    
     
